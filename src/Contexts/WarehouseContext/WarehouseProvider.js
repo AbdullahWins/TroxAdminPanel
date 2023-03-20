844c544601e02375b2585820271faa5e551ebca4
Abdullah Al MahMud <abdudevs@gmail.com>
2023-03-20 11:51:08 +0600

    added warehouse add page with crud

--- src/Contexts/WarehouseContext/WarehouseProvider.js
+++ src/Contexts/WarehouseContext/WarehouseProvider.js
@@ -1,11 +1,14 @@
 import React, { createContext, useEffect, useState } from "react";
 import { firebaseFirestore } from "../../Firebase/firebase.config";
+import { v4 as uuidv4 } from "uuid";
 import {
   collection,
   doc,
   getDoc,
   //   getDoc,
   getDocs,
+  serverTimestamp,
+  setDoc,
   //   serverTimestamp,
   //   setDoc,
   updateDoc,
@@ -30,7 +33,7 @@ const WarehouseProvider = ({ children }) => {
       const warehouseDocRef = doc(db, "warehouseDetails", warehouse);
       try {
         await updateDoc(warehouseDocRef, {
-          user_status: status,
+          warehouse_status: status,
         });
         fetchWarehouses();
         console.log("warehouse status updated successfully");
@@ -55,7 +58,7 @@ const WarehouseProvider = ({ children }) => {
       const docSnap = await getDoc(ref);
       if (docSnap.exists()) {
         const warehouse = docSnap.data();
-        if (currentWarehouse?.user_id === warehouse?.warehouse_id) {
+        if (currentWarehouse?.warehouse_id === warehouse?.warehouse_id) {
           return;
         } else {
           setCurrentWarehouse(warehouse);
@@ -73,16 +76,17 @@ const WarehouseProvider = ({ children }) => {
   const updateSingleWarehouse = async (newWarehouse, id) => {
     try {
       const db = firebaseFirestore;
-      const warehouseDocRef = doc(db, "userDetails", id);
+      const warehouseDocRef = doc(db, "warehouseDetails", id);
       try {
         await updateDoc(warehouseDocRef, {
-          user_name: newWarehouse?.user_name,
-          user_email: newWarehouse?.user_email,
-          user_contact: newWarehouse?.user_contact,
-          user_dob: newWarehouse?.user_dob,
-          user_gender: newWarehouse?.user_gender,
-          user_country: newWarehouse?.user_country,
-          user_address: newWarehouse?.user_address,
+          warehouse_name: newWarehouse?.warehouse_name,
+          warehouse_email: newWarehouse?.warehouse_email,
+          warehouse_password: newWarehouse?.warehouse_password,
+          warehouse_password_confirm: newWarehouse?.warehouse_password_confirm,
+          warehouse_country: newWarehouse?.warehouse_country,
+          warehouse_state: newWarehouse?.warehouse_state,
+          warehouse_city: newWarehouse?.warehouse_city,
+          warehouse_status: newWarehouse?.warehouse_status,
         });
         fetchWarehouses();
         console.log("warehouse updated successfully");
@@ -115,37 +119,34 @@ const WarehouseProvider = ({ children }) => {
   //   };
 
   // add one warehouse
-  //   const addOneWarehouse = async (newWarehouse, images) => {
-  //     try {
-  //       const db = firebaseFirestore;
-  //       const warehouseId = uuidv4();
-  //       const timeStamp = serverTimestamp();
-  //       const warehouseDocRef = doc(db, "userDetails", warehouseId);
-  //       try {
-  //         await setDoc(warehouseDocRef, {
-  //           rider_id: warehouseId,
-  //           rider_name: newWarehouse?.rider_name,
-  //           rider_email: newWarehouse?.rider_email,
-  //           rider_contact: newWarehouse?.rider_contact,
-  //           rider_dob: newWarehouse?.rider_dob,
-  //           rider_gender: newWarehouse?.rider_gender,
-  //           rider_country: newWarehouse?.rider_country,
-  //           rider_state: newWarehouse?.rider_state,
-  //           rider_work_location: newWarehouse?.rider_work_location,
-  //           rider_address: newWarehouse?.rider_address,
-  //           rider_status: newWarehouse?.rider_status,
-  //           timestamp: timeStamp,
-  //           rider_documents: await uploadImages(images),
-  //         });
-  //         fetchwarehouses();
-  //         console.log("warehouse successfully added");
-  //       } catch (error) {
-  //         console.error("Error adding warehouse", error);
-  //       }
-  //     } catch (error) {
-  //       console.error("Error adding warehouse", error);
-  //     }
-  //   };
+  const addOneWarehouse = async (newWarehouse) => {
+    try {
+      const db = firebaseFirestore;
+      const warehouseId = uuidv4();
+      const timeStamp = serverTimestamp();
+      const warehouseDocRef = doc(db, "userDetails", warehouseId);
+      try {
+        await setDoc(warehouseDocRef, {
+          warehouse_id: warehouseId,
+          warehouse_name: newWarehouse?.warehouse_name,
+          warehouse_email: newWarehouse?.warehouse_email,
+          warehouse_password: newWarehouse?.warehouse_password,
+          warehouse_password_confirm: newWarehouse?.warehouse_password_confirm,
+          warehouse_country: newWarehouse?.warehouse_country,
+          warehouse_state: newWarehouse?.warehouse_state,
+          warehouse_city: newWarehouse?.warehouse_city,
+          warehouse_status: newWarehouse?.warehouse_status,
+          timestamp: timeStamp,
+        });
+        fetchWarehouses();
+        console.log("warehouse successfully added");
+      } catch (error) {
+        console.error("Error adding warehouse", error);
+      }
+    } catch (error) {
+      console.error("Error adding warehouse", error);
+    }
+  };
 
   //fetch warehouses from database
   const fetchWarehouses = async () => {
@@ -174,7 +175,9 @@ const WarehouseProvider = ({ children }) => {
       setFilteredWarehousesBySearch(warehouses);
     }
     const filteredWarehouses = warehouses?.filter((warehouse) =>
-      warehouse?.user_name?.toLowerCase().includes(searchValue.toLowerCase())
+      warehouse?.warehouse_name
+        ?.toLowerCase()
+        .includes(searchValue.toLowerCase())
     );
     setFilteredWarehousesBySearch(filteredWarehouses);
     setSearchBarValue(searchValue);
@@ -217,7 +220,7 @@ const WarehouseProvider = ({ children }) => {
     fetchSingleWarehouse,
     updateSingleWarehouse,
     warehouses,
-    // addOneWarehouse,
+    addOneWarehouse,
     setWarehouses,
     searchBarValue,
     setSearchBarValue,
